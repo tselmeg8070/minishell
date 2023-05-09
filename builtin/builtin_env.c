c436f15b8d0602f8b3b8c79f553d2ed54daa1294
Gereltuya <gereltuya.altangerel@gmail.com>
2023-05-09 17:46:09 +0200

    env fixed

--- builtin/builtin_env.c
+++ builtin/builtin_env.c
@@ -24,18 +24,46 @@ int	ft_env(t_env_list **envs)
 	tmp = *envs;
 	while (tmp->next)
 	{
-		if (ft_strcmp(tmp->key, "$?"))
+		if (!ft_strcmp(tmp->key, "$?"))
 			tmp = tmp->next;
 		else
 		{
-			if (tmp->val != NULL)
+			if (tmp->val)
 			{
-				write (1, tmp->key, ft_strlen(tmp->key));
-				write (1, "=", 1);
-				write (1, tmp->val, ft_strlen(tmp->val));
+				ft_putstr_fd(tmp->key, 1);
+				write(1, "=", 1);
+				ft_putstr_fd(tmp->val, 1);
+				write(1, "\n", 1);
 			}
 			tmp = tmp->next;
 		}
 	}
 	return (0);
 }
+
+// {
+// 	t_env_list	*tmp;
+
+// 	if (!envs || !*envs)
+// 	{
+// 		write(2, "minishell: env: No such file or directory\n", 43);
+// 		return (127);
+// 	}
+// 	tmp = *envs;
+// 	while (tmp->next)
+// 	{
+// 		if (ft_strcmp(tmp->key, "$?"))
+// 			tmp = tmp->next;
+// 		else
+// 		{
+// 			if (tmp->val != NULL)
+// 			{
+// 				write (1, tmp->key, ft_strlen(tmp->key));
+// 				write (1, "=", 1);
+// 				write (1, tmp->val, ft_strlen(tmp->val));
+// 			}
+// 			tmp = tmp->next;
+// 		}
+// 	}
+// 	return (0);
+// }
