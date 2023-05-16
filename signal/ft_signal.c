043265606c13905f9def963a2a3bf4fd88ddf195
Gereltuya <gereltuya.altangerel@gmail.com>
2023-05-16 20:50:38 +0200

    ctrl+z ignored

--- signal/ft_signal.c
+++ signal/ft_signal.c
@@ -12,27 +12,43 @@
 
 #include "../minishell.h"
 
-void	handler(int sig)
+void	parent_handler(int sig)
 {
-	if (sig == SIGINT)
+	ft_putnbr_fd(g_status, 1);
+	if (g_status == 1)
+	{
+		ft_putstr_fd("here\n", 1);
+		// g_status = sig + 128;
+		signal(sig, SIG_IGN);
+		exit(sig + 128);
+	}
+	else
 	{
 		write(2, "\n", 1);
 		rl_replace_line("", 0);
 		rl_on_new_line();
 		ft_history("");
+		g_status = sig + 128;
 	}
-	if (sig == SIGQUIT)
-	{
-		rl_on_new_line();
-		ft_history("");
-	}
+}
+
+void	child_handler(int sig)
+{
+	// if (sig == SIGINT)
+	// 	signal(SIGINT, SIG_IGN);
+	exit(sig + 128);
 }
 
 void	ft_init_sig(struct sigaction *sa)
 {
 	sigemptyset(&sa->sa_mask);
-	sa->sa_flags = SA_SIGINFO;
-	sa->sa_handler = &handler;
-	sigaction(SIGINT, sa, NULL);
-	sigaction(SIGQUIT, sa, NULL);
+	sa->sa_flags = SA_RESTART;
+	sa->sa_handler = &parent_handler;
+	if (sigaction(SIGINT, sa, NULL) == -1)
+	{
+		ft_putstr_fd("Sigaction error!\n", 2);
+		exit(1);
+	}
+	signal(SIGQUIT, SIG_IGN);
+	signal(SIGTSTP, SIG_IGN);
 }
