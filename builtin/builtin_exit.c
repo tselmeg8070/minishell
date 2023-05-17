6afecb7e269fc42d7ab587c19df90114150d42c6
Gereltuya <gereltuya.altangerel@gmail.com>
2023-05-17 21:45:07 +0200

    exit err

--- builtin/builtin_exit.c
+++ builtin/builtin_exit.c
@@ -12,76 +12,95 @@
 
 #include "../minishell.h"
 
-int	ft_all_digits(char **str)
+int	ft_all_digits(char *str)
 {
 	int	i;
-	int	j;
 
 	i = 0;
-	while (str[i])
+	if (str[i] == '+' || str[i] == '-')
+		i++;
+	while (str[i] != '\0')
 	{
-		j = 0;
-		while (str[i][j] != '\0')
+		if (!ft_isdigit(str[i]))
 		{
-			if (!ft_isdigit(str[i][j]))
-			{
-				write(2, "minishell: exit: ", 18);
-				ft_putstr_fd(str[i], 2);
-				write(2, ": numeric argument required\n", 29);
-				return (0);
-			}
-			j++;
+			write(2, "minishell: exit: ", 18);
+			ft_putstr_fd(str, 2);
+			write(2, ": numeric argument required\n", 29);
+			return (0);
 		}
 		i++;
 	}
 	return (1);
 }
 
-//exit: bash: exit: too many arguments - dont exit
-int	ft_exit_bt(char	**str)
+unsigned long long	ft_long_atoi(char *str)
 {
-	int	exit_val;
+	unsigned long long	nb;
 
-	exit_val = 0;
+	if (!str)
+		return (1);
+	nb = 0;
+	if (*str == '-' || *str == '+')
+		str++;
+	while (*str != '\0')
+	{
+		if (*str >= '0' && *str <= '9')
+			nb = nb * 10 + (*str - '0');
+		str++;
+	}
+	return (nb);
+}
+
+int	ft_exit_calc(char *str)
+{
+	unsigned long long	val;
+	long long			return_val;
+
+	if (!str)
+		return (1);
+	if (!ft_strcmp(str, "-9223372036854775808"))
+		val = 0;
+	else
+		val = ft_long_atoi(str);
+	if (9223372036854775807 < val)
+	{
+		write(2, "minishell: exit: ", 18);
+		ft_putstr_fd(str, 2);
+		write(2, ": numeric argument required\n", 29);
+		return (2 + 256);
+	}
+	return_val = 1;
+	if (*str == '-')
+		return_val = -1;
+	return_val = (val * return_val) % 256;
+	return (return_val + 256);
+}
+
+int	ft_exit_bt(char **str)
+{
 	if (!str || !*str)
 		return (1);
 	if (str && !ft_strcmp(*str, "exit"))
 		str++;
+	write (1, "exit\n", 5);
 	if (!*str)
-	{
-		write (1, "exit\n", 5);
 		return (0 + 256);
-	}
-	else if (str && ft_count_strs(str) > 1)
+	if (str && ft_count_strs(str) == 1)
 	{
-		write (1, "exit\n", 5);
-		write(2, "minishell: exit: too many arguments\n", 37);
-		return (1 + 256);
+		if (!ft_all_digits(*str))
+			return (2 + 256);
+		else
+			return (ft_exit_calc(*str));
 	}
-	else if (str && !ft_all_digits(str))
-		return (2 + 256);
-	else if (str && ft_all_digits(str) == 1)
-		exit_val = ft_atoi(*str) % 256;
-	return (exit_val);
+	else
+	{
+		if (!ft_all_digits(*str))
+			return (2 + 256);
+		else
+		{
+			write(2, "minishell: exit: too many arguments\n", 37);
+			return (1 + 256);
+		}
+	}
+	return (0);
 }
-
-// int	ft_exit_bt(char **str)
-// {
-// 	int	exit_val;
-
-// 	write(2, "exit\n", 5);
-// 	if (str && !ft_all_digits(str))
-// 		return (2);
-// 	else if (str && ft_count_strs(str) > 1)
-// 	{
-// 		write(2, "minishell: exit: too many arguments\n", 37);
-// 		return (1);
-// 	}
-// 	else if (!str || !*str)
-// 	{
-// 		return (0);
-// 	}
-// 	else
-// 		exit_val = ft_atoi(*str) % 256;
-// 	return (exit_val);
-// }
